5a0b6324032d8323b6bed83a5cd15879714e01db
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-17 23:15:01 +0800

    feat: add findall in movie session service

---
+++ src/test/java/com/JAPKAM/Movieverse/MovieSessionServiceTests.java
@@ -0,0 +1,98 @@
+package com.JAPKAM.Movieverse;
+
+import com.JAPKAM.Movieverse.entity.*;
+import com.JAPKAM.Movieverse.repository.MovieSessionRepository;
+import com.JAPKAM.Movieverse.service.MovieSessionService;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class MovieSessionServiceTests {
+    public static final String ACTION_TAG = "action";
+    public static final String ROMANTIC_TAG = "romantic";
+    public static final String MOVIE_1_NAME = "Movie 1";
+    public static final String MOVIE_2_NAME = "Movie 2";
+    public static final Date TIMESLOT_ONE = new Date(2022,12,16,16,30);
+    public static final Date TIMESLOT_TWO = new Date(2022,12,17,17,30);
+    public static final String HOUSE_ONE = "HOUSE ONE";
+    public static final int HOUSE_ONE_ROW_NUMBER = 20;
+    public static final int HOUSE_ONE_COL_NUMBER = 20;
+    public static final String HOUSE_TWO = "HOUSE TWO";
+    public static final int HOUSE_TWO_ROW_NUMBER = 5;
+    public static final int HOUSE_TWO_COL_NUMBER = 10;
+    public static final double MOVIE_1_PRICE = 80;
+    public static final double MOVIE_2_PRICE = 90;
+
+    @Mock
+    MovieSessionRepository movieSessionRepository;
+
+    @InjectMocks
+    MovieSessionService movieSessionService;
+
+    @Test
+    void should_return_all_movie_session_when_find_all_given_movie_sessions() {
+        //given
+        List<Tag> tags = new ArrayList<>();
+        List<Tag> tags1 = Arrays.asList(new Tag(new ObjectId().toString(), ACTION_TAG));
+        List<Tag> tags2 = Arrays.asList(new Tag(new ObjectId().toString(), ROMANTIC_TAG));
+
+        Movie movie1 = new Movie(new ObjectId().toString(), MOVIE_1_NAME, tags1,null);
+        Movie movie2 = new Movie(new ObjectId().toString(), MOVIE_2_NAME, tags2,null);
+
+        Timeslot timeslot1 = new Timeslot(new ObjectId().toString(), TIMESLOT_ONE);
+        Timeslot timeslot2 = new Timeslot(new ObjectId().toString(), TIMESLOT_TWO);
+
+        House house1 = new House(new ObjectId().toString(), HOUSE_ONE, HOUSE_ONE_ROW_NUMBER, HOUSE_ONE_COL_NUMBER);
+        House house2 = new House(new ObjectId().toString(), HOUSE_TWO, HOUSE_TWO_ROW_NUMBER, HOUSE_TWO_COL_NUMBER);
+
+        List<Seat> seats1 = new ArrayList<>();
+        for(int i = 0 ; i < house1.getNumberOfRow(); i++){
+            for(int j =0 ;j <house1.getNumberOfColumn(); j++) {
+                seats1.add(new Seat(new ObjectId().toString(), i+1, j+1, SeatStatus.AVAILABLE));
+            }
+        }
+
+        List<Seat> seats2 = new ArrayList<>();
+        for(int i = 0 ; i < house2.getNumberOfRow(); i++){
+            for(int j =0 ;j <house2.getNumberOfColumn(); j++) {
+                seats2.add(new Seat(new ObjectId().toString(), i+1, j+1, SeatStatus.AVAILABLE));
+            }
+        }
+
+        MovieSession movieSession1 = new MovieSession(new ObjectId().toString(), movie1, timeslot1,
+                house1, MOVIE_1_PRICE, seats1);
+        MovieSession movieSession2 = new MovieSession(new ObjectId().toString(), movie2, timeslot2,
+                house2, MOVIE_2_PRICE, seats2);
+
+        List<MovieSession> movieSessions = Arrays.asList(movieSession1, movieSession2);
+
+        when(movieSessionRepository.findAll()).thenReturn(movieSessions);
+        //when
+
+        List<MovieSession> returnedMoviesSessions = movieSessionService.findAll();
+
+        //then
+        assertThat(returnedMoviesSessions, hasSize(2));
+        assertThat(returnedMoviesSessions.get(0), equalTo(movieSession1));
+        assertThat(returnedMoviesSessions.get(1), equalTo(movieSession2));
+        verify(movieSessionRepository).findAll();
+    }
+
+
+
+}

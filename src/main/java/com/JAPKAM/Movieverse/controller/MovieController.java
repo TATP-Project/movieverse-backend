fadb0a5840df53c5cd8a731f7c37a0f6b70c2564
leungpo <polly.leung@oocl.com>
2022-12-17 23:05:24 +0800

    feat: add MovieController and getAll API

---
+++ src/main/java/com/JAPKAM/Movieverse/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.JAPKAM.Movieverse.controller;
+
+import com.JAPKAM.Movieverse.entity.Movie;
+import com.JAPKAM.Movieverse.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getAll() {
+        return movieService.findAll();
+    }
+}
---
+++ src/test/java/com/JAPKAM/Movieverse/MovieControllerTest.java
@@ -0,0 +1,62 @@
+package com.JAPKAM.Movieverse;
+
+import com.JAPKAM.Movieverse.entity.Movie;
+import com.JAPKAM.Movieverse.entity.Tag;
+import com.JAPKAM.Movieverse.repository.MovieRepository;
+import org.bson.types.Binary;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MovieControllerTest {
+
+    @Autowired
+    MockMvc client;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @BeforeEach
+    public void clearDB() {
+        movieRepository.deleteAll();
+    }
+
+    @Test
+    void should_get_all_movies_when_perform_get_given_2_movies() throws Exception {
+        //given
+        List<Tag> tags = new ArrayList<>();
+        Tag tag1 = new Tag(new ObjectId().toString(),"action");
+        Tag tag2 = new Tag(new ObjectId().toString(),"romantic");
+        tags.add(tag1);
+        tags.add(tag2);
+        Binary image1 = new Binary(new byte[1]);
+        Binary image2 = new Binary(new byte[1]);
+        Movie movie1 = new Movie(new ObjectId().toString(),"Movie 1", tags,image1);
+        Movie movie2 = new Movie(new ObjectId().toString(),"Movie 2", tags,image2);
+        movieRepository.save(movie1);
+        movieRepository.save(movie2);
+        //when & then
+
+        client.perform(MockMvcRequestBuilders.get("/movies"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(2)))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Movie 1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Movie 2"));
+    }
+}

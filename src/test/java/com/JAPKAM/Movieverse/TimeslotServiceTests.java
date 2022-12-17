fe3f55990ebfe116eaeee50de0252d61a3d564a9
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-17 22:53:16 +0800

    test: use mock in service test

--- src/test/java/com/JAPKAM/Movieverse/TimeslotServiceTests.java
+++ src/test/java/com/JAPKAM/Movieverse/TimeslotServiceTests.java
@@ -6,30 +6,31 @@ import com.JAPKAM.Movieverse.service.TimeslotService;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class TimeslotServiceTests {
 
-    @Autowired
+    @Mock
     TimeslotMongoRepository timeslotMongoRepository;
 
-    @Autowired
+    @InjectMocks
     TimeslotService timeslotService;
-
-    @BeforeEach
-    public void clearDB(){
-        timeslotMongoRepository.deleteAll();
-    }
 
     public static final Date TIMESLOT_ONE = new Date(2022,12,16,16,30);
     public static final Date TIMESLOT_TWO = new Date(2022,12,17,17,30);
@@ -43,37 +44,35 @@ public class TimeslotServiceTests {
         Timeslot timeslot2 = new Timeslot(new ObjectId().toString(), TIMESLOT_TWO);
         Timeslot timeslot3 = new Timeslot(new ObjectId().toString(), TIMESLOT_THREE);
         Timeslot timeslot4 = new Timeslot(new ObjectId().toString(), TIMESLOT_FOUR);
-        timeslotMongoRepository.saveAll(Arrays.asList(timeslot1, timeslot2, timeslot3, timeslot4));
+
+        List<Timeslot> timeslots = Arrays.asList(timeslot1, timeslot2, timeslot3, timeslot4);
+        when(timeslotMongoRepository.findAll()).thenReturn(timeslots);
         //when
 
-        List<Timeslot> timeslots = timeslotService.findAll();
+        List<Timeslot> returnedTimeslots = timeslotService.findAll();
         //then
-        assertThat(timeslots, hasSize(4));
-        assertThat(timeslots.get(0).getId(), equalTo(timeslot1.getId()));
-        assertThat(timeslots.get(0).getStartDateTime(), equalTo(timeslot1.getStartDateTime()));
-        assertThat(timeslots.get(1).getId(), equalTo(timeslot2.getId()));
-        assertThat(timeslots.get(1).getStartDateTime(), equalTo(timeslot2.getStartDateTime()));
-        assertThat(timeslots.get(2).getId(), equalTo(timeslot3.getId()));
-        assertThat(timeslots.get(2).getStartDateTime(), equalTo(timeslot3.getStartDateTime()));
-        assertThat(timeslots.get(3).getId(), equalTo(timeslot4.getId()));
-        assertThat(timeslots.get(3).getStartDateTime(), equalTo(timeslot4.getStartDateTime()));
+        assertThat(returnedTimeslots, hasSize(4));
+        assertThat(returnedTimeslots.get(0), equalTo(timeslot1));
+        assertThat(returnedTimeslots.get(1), equalTo(timeslot2));
+        assertThat(returnedTimeslots.get(2), equalTo(timeslot3));
+        assertThat(returnedTimeslots.get(3), equalTo(timeslot4));
+        verify(timeslotMongoRepository).findAll();
 
     }
 
     @Test
     void should_return_timeslot_3_when_find_by_id_given_id() throws Exception {
         //given
-        Timeslot timeslot1 = new Timeslot(new ObjectId().toString(), TIMESLOT_ONE);
-        Timeslot timeslot2 = new Timeslot(new ObjectId().toString(), TIMESLOT_TWO);
+
         Timeslot timeslot3 = new Timeslot(new ObjectId().toString(), TIMESLOT_THREE);
-        Timeslot timeslot4 = new Timeslot(new ObjectId().toString(), TIMESLOT_FOUR);
-        timeslotMongoRepository.saveAll(Arrays.asList(timeslot1, timeslot2, timeslot3, timeslot4));
+
+        when(timeslotMongoRepository.findById(timeslot3.getId())).thenReturn(Optional.of(timeslot3));
         //when
 
         Timeslot returnedTimeslot = timeslotService.findById(timeslot3.getId());
         //then
 
-        assertThat(returnedTimeslot.getId(), equalTo(timeslot3.getId()));
-        assertThat(returnedTimeslot.getStartDateTime(), equalTo(timeslot3.getStartDateTime()));
+        assertThat(returnedTimeslot,equalTo(timeslot3));
+        verify(timeslotMongoRepository).findById(timeslot3.getId());
     }
 }

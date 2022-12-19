db0b049b7da6eab22b8599b0bc30e08da6419708
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-19 14:28:51 +0800

    feat: add find all movie sessions by movie id

--- src/main/java/com/JAPKAM/Movieverse/repository/MovieSessionRepository.java
+++ src/main/java/com/JAPKAM/Movieverse/repository/MovieSessionRepository.java
@@ -4,6 +4,9 @@ import com.JAPKAM.Movieverse.entity.MovieSession;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieSessionRepository extends MongoRepository<MovieSession, String> {
+    List<MovieSession> findByMovieId(String id);
 }

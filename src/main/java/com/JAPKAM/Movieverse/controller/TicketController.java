5cb740ff1825b8bad99d44f13fa97e96845e8b3e
tokaho <tokaho2000@gmail.com>
2022-12-20 13:05:23 +0800

    [Feat] ticket API framework

--- src/main/java/com/JAPKAM/Movieverse/controller/TicketController.java
+++ src/main/java/com/JAPKAM/Movieverse/controller/TicketController.java
@@ -1,4 +1,23 @@
 package com.JAPKAM.Movieverse.controller;
 
+import com.JAPKAM.Movieverse.entity.Ticket;
+import com.JAPKAM.Movieverse.service.TicketService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tickets")
 public class TicketController {
+    private TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping
+    public String postTicket(@RequestBody Ticket ticket){
+        return ticketService.postTicket(ticket);
+    }
 }
